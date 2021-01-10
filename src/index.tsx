901f5b85a701c6f279e72e53b814b9c201754c1f
Tone Yu <tone.yu@accenture.com>
2021-01-09 19:04:37 -0600

    Add chakra

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,12 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 import './App.global.css';
 
-render(<App />, document.getElementById('root'));
+render(
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>,
+  document.getElementById('root')
+);
